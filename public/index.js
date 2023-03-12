759232a02811110bb634c64704793add88d7c67b
leopico <linkoko.peceng@gmail.com>
2023-03-12 17:27:33 +0630

    first-commit

---
+++ public/index.js
@@ -0,0 +1,10 @@
+const pizza = document.querySelector("#pizza");
+const menu = document.querySelector("#menu");
+
+pizza.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
